4e361748879a8e4f0ab99f4fd24fcee5d44476b8
richardhowez <richard-howes@live.com>
2024-01-31 16:03:24 -0700

    Added Model Prediction and exploration features
    Next step would be bringing it all together with some data engineering around a specific problem.
    
    Neo4J DB set up
    AWS, Azure or GCP set up for pipelines, orchestration and deployment

--- bot.py
+++ bot.py
@@ -3,41 +3,50 @@ from utils import write_message
 from llm import llm, embeddings
 # from graph import graph
 from agent import generate_response
+from predict_page import show_predict_page
+from explore_page import show_explore_page
 
 # tag::setup[]
 # Page Config
 st.set_page_config("Ebert", page_icon=":movie_camera:")
 # end::setup[]
 
-# tag::session[]
-# Set up Session State
-if "messages" not in st.session_state:
-    st.session_state.messages = [
-        {"role": "assistant", "content": "Hi, I'm the GraphAcademy Chatbot!  How can I help you?"},
-    ]
-# end::session[]
+page = st.sidebar.selectbox("Explore, Predict or Query", ("Predict", "Explore", "Query"))
 
-# tag::submit[]
-# Submit handler
-def handle_submit(message):
-    # Handle the response
-    with st.spinner('Thinking...'):
+if page == "Predict":
+    show_predict_page()
+if page == "Explore":
+    show_explore_page()
+else:
+    # tag::session[]
+    # Set up Session State
+    if "messages" not in st.session_state:
+        st.session_state.messages = [
+            {"role": "assistant", "content": "Hi, I'm the GraphAcademy Chatbot!  How can I help you?"},
+        ]
+    # end::session[]
 
-        response = generate_response(message)
-        write_message('assistant', response)
-# end::submit[]
+    # tag::submit[]
+    # Submit handler
+    def handle_submit(message):
+        # Handle the response
+        with st.spinner('Thinking...'):
+
+            response = generate_response(message)
+            write_message('assistant', response)
+    # end::submit[]
 
 
-# tag::chat[]
-# Display messages in Session State
-for message in st.session_state.messages:
-    write_message(message['role'], message['content'], save=False)
+    # tag::chat[]
+    # Display messages in Session State
+    for message in st.session_state.messages:
+        write_message(message['role'], message['content'], save=False)
 
-# Handle any user input
-if prompt := st.chat_input("What is up?"):
-    # Display user message in chat message container
-    write_message('user', prompt)
+    # Handle any user input
+    if prompt := st.chat_input("What is up?"):
+        # Display user message in chat message container
+        write_message('user', prompt)
 
-    # Generate a response
-    handle_submit(prompt)
-# end::chat[]
+        # Generate a response
+        handle_submit(prompt)
+    # end::chat[]
