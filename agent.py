921949030d9cf92e1ee70dd4c61e057e9ee31b64
richardhowez <richard-howes@live.com>
2024-01-31 09:49:11 -0700

    Final updates to the project

---
+++ agent.py
@@ -0,0 +1,102 @@
+from langchain.agents import AgentExecutor, create_react_agent
+from langchain import hub
+from llm import llm 
+from langchain.tools import Tool    
+from langchain.prompts import PromptTemplate
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+from tools.vector import kg_qa
+from tools.cypher import cypher_qa
+
+# Include the LLM from a previous lesson
+from llm import llm
+# tag::tools[]
+tools = [
+    Tool.from_function(
+        name="General Chat",
+        description="For general chat not covered by other tools",
+        func=llm.invoke,
+        return_direct=False
+    ),
+    Tool.from_function(
+        name="Cypher QA",
+        description="Provide information about movies questions using Cypher",
+        func = cypher_qa,
+        return_direct=False
+    ),
+    Tool.from_function(
+        name="Vector Search Index",
+        description="Provides information about movie plots using Vector Search",
+        func = kg_qa,
+        return_direct=False
+    )
+]
+# end::tools[]
+
+
+memory = ConversationBufferWindowMemory(
+    memory_key='chat_history',
+    k=5,
+    return_messages=True,
+)
+
+#agent_prompt = hub.pull("hwchase17/react-chat")
+agent_prompt = PromptTemplate.from_template("""
+You are a movie expert providing information about movies.
+Be as helpful as possible and return as much information as possible.
+Do not answer any questions that do not relate to movies, actors or directors.
+Respond to all questions in pirate speak.
+TOOLS:
+------
+
+You have access to the following tools:
+
+{tools}
+
+To use a tool, please use the following format:
+
+```
+Thought: Do I need to use a tool? Yes
+Action: the action to take, should be one of [{tool_names}]
+Action Input: the input to the action
+Observation: the result of the action
+```
+
+When you have a response to say to the Human, or if you do not need to use a tool, you MUST use the format:
+
+```
+Thought: Do I need to use a tool? No
+Final Answer: [your response here]
+```
+
+Begin!
+
+Previous conversation history:
+{chat_history}
+
+New input: {input}
+{agent_scratchpad}
+""")
+agent = create_react_agent(llm, tools, agent_prompt)
+agent_executor = AgentExecutor(
+    agent=agent,
+    tools=tools,
+    memory=memory,
+    handle_parsing_errors=True,
+    verbose=True
+    )
+
+
+
+
+# tag::generate_response[]
+def generate_response(prompt):
+    """
+    Create a handler that calls the Conversational agent
+    and returns a response to be rendered in the UI
+    """
+
+    response = agent_executor.invoke({"input": prompt})
+
+    return response['output']
+# end::generate_response[]
+
