56b1224d20978f90722ea6c3efc186e29b7ec8de
richardhowez <richard-howes@live.com>
2024-01-31 17:10:09 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,8 @@ page = "Explore"
 page = st.sidebar.selectbox("Explore, Predict or Query", ("Predict", "Explore", "Query"))
 
 if page == "Predict":
-    show_predict_page()
+    #show_predict_page()
+    show_explore_page()
 if page == "Explore":
     show_explore_page()
 if page == "Query":
